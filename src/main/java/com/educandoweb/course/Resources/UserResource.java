a0f04e9922813166948cf5d844344b31c7309d1d
ErikRodrigues-Dev <erik.rodrigues.dev@gmail.com>
2023-10-19 10:42:30 -0300

    Order, instant ISO8601- Parte1

--- src/main/java/com/educandoweb/course/Resources/UserResource.java
+++ src/main/java/com/educandoweb/course/Resources/UserResource.java
@@ -2,7 +2,6 @@ package com.educandoweb.course.Resources;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
